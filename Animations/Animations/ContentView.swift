c7c4b803e158c5e17202f294c6ae2d25acb239bc
Mariana Yamamoto <myamamot@corp.lifescan.com>
2021-09-09 11:04:28 -0400

    initial project

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Mariana Yamamoto on 9/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
