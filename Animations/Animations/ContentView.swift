af9454292b6f9a1dcde60246f44c279986269b5c
Mariana Yamamoto <myamamot@corp.lifescan.com>
2021-09-09 11:17:41 -0400

    Add animations effects on button

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+//            self.animationAmount += 1
+        }
+        .padding(40)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation.easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+//        .animation(.interpolatingSpring(stiffness: 50, damping: 1))
+//        .animation(
+//            Animation.easeInOut(duration: 2)
+//                .delay(1)
+//        )
+//        .animation(
+//            Animation.easeInOut(duration: 1)
+//                .repeatCount(2, autoreverses: true)
+//        )
+//        .animation(
+//            Animation.easeInOut(duration: 1)
+//                .repeatForever(autoreverses: true)
+//        )
+        .onAppear {
+            self.animationAmount = 2
+        }
     }
 }
 
